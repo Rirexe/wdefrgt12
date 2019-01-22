d588af246f74a2a067735bb18d91975188a568e9
Rirexe <45144673+Rirexe@users.noreply.github.com>
2019-01-22 13:47:56 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("537206609584193549")
+setInterval(function() {
+channel.send(`farm credits farmedits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits farm credits   `);
+}, 30)
+})
+
+client.login("NTM3MjIwMTQ2NjA4MjA5OTIw.DyiFoQ.o5_8VFN_2jRSaugpjserWhAkLJc");
